3cc497a53fa3167286710c6e1fa4c671014dba11
Johnnie-Hernandez <johnnie.hernandez@ibm.com>
2022-06-21 13:08:43 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "cp4s_endpoints" {
   depends_on = [
-    data.external.get_cp4s_endpoints,
+    data.external.get_cp4s_endpoint,
   ]
   value = length(data.external.get_cp4s_endpoints) > 0 ? data.external.get_cp4s_endpoints.result.endpoint : ""
 }
